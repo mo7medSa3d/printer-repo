fd771dc718af1604d5e025bf5de3028475bd899d
Mohamed Saad <111496655+mo7medSa3d@users.noreply.github.com>
2026-09-06 15:48:49 +0300

    fix(odoo): load native branch ownership bridge after legacy branch model

--- odoo_addons/print_gateway/models/__init__.py
+++ odoo_addons/print_gateway/models/__init__.py
@@ -10,5 +10,6 @@ from . import report_mapping
 from . import ir_actions_report
 from . import branch_security
 from . import branch_multicompany
+from . import native_branch_bridge
 from . import async_report
 from . import odoo19_compat
