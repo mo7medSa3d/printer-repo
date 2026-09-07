61ce25785275b5ade6576652e12c69e85ec6e582
Mohamed Saad <111496655+mo7medSa3d@users.noreply.github.com>
2026-09-07 10:22:01 +0300

    fix(pos): use Odoo 19 PosStore module path

--- odoo_addons/print_gateway/static/src/js/pos_print_router.js
+++ odoo_addons/print_gateway/static/src/js/pos_print_router.js
@@ -1,7 +1,7 @@
 /** @odoo-module */
 
 import { patch } from "@web/core/utils/patch";
-import { PosStore } from "@point_of_sale/app/store/pos_store";
+import { PosStore } from "@point_of_sale/app/services/pos_store";
 
 patch(PosStore.prototype, {
     /**
