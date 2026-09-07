b1f4088718337dfb91c39ba562cd4084748549ac
Mohamed Saad <111496655+mo7medSa3d@users.noreply.github.com>
2026-09-07 11:44:29 +0300

    fix: remove unused React import from HeaderNav

--- src/components/HeaderNav.tsx
+++ src/components/HeaderNav.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import { LayoutDashboard, Menu, X, Home, LogOut, KeyRound } from "lucide-react";
